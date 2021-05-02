d68419ef90005795a874c7890eaa78882e2f1e28
Max Stevens <max@fishlick.com>
2021-05-02 11:40:40 -0700

    Turns out that with ipv4 you can't guarantee that ifconfig_remote is actually passed.  If a subnet is being used, it won't be.  However, at least Linux doesn't need this for the route table - you can simply add a default route via the device, no need to specify the remote ip.

--- vpn-up.sh
+++ vpn-up.sh
@@ -8,5 +8,4 @@ rt_table="piatunnel"
 # echo 200 piatunnel >> /etc/iproute2/rt_tables
 
 ip rule add from "$ifconfig_local" table "$rt_table"
-ip route add table "$rt_table" default via "$ifconfig_remote"
-ip route add table "$rt_table" "$ifconfig_remote" via "$ifconfig_local" dev "$dev"
+ip route add table "$rt_table" default via "$ifconfig_local" dev "$dev"
